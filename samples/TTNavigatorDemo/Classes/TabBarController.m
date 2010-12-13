f217aee29568a0854950a1e95ec21f40c643b6ac
Cemal Eker <hello@cemaleker.com>
2010-12-13 14:58:48 +0200

    [TTNavigator]Showcase for a TTNavigator bug for views under moreViewController

--- samples/TTNavigatorDemo/Classes/TabBarController.m
+++ samples/TTNavigatorDemo/Classes/TabBarController.m
@@ -11,6 +11,9 @@
                                              @"tt://menu/3",
                                              @"tt://menu/4",
                                              @"tt://menu/5",
+                                             @"tt://menu/5",
+                                             @"tt://menu/4",
+                                             @"tt://menu/3",
                                              nil]];
 }
 
